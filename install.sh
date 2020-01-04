1f79106fa728ee0b29b81aae4020da5974dd8415
strnh <Sotaro.Hikosaka@gmail.com>
2020-01-05 02:05:02 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DATE=`date +%Y%m%d`
 SPAMCNTDIR=/usr/ports/mail/qmail-spamcontrol
 QMAILDIR=/usr/ports/mail/qmail
 
-cp ./extra-patch-spamcontrol-tls_timeout.c $QMAILDIR/files
+cp -p extra-patch-spamcontrol-tls_timeout.c $QMAILDIR/files
 mv $SPAMCNTDIR/Makefile $SPAMCONTDIR/Makefile.orig_$DATE
 cp qmail-spamcontrol/Makefile $SPAMCNTDIR/Makefile
 mv $QMAILDIR/Makefile $QMAILDIR/Makefile.orig_$DATE
