dcfb72b3504900fb851b14aa132e4c56231d8c5b
strnh <Sotaro.Hikosaka@gmail.com>
2022-06-13 21:03:55 +0900

    Update install.sh
    merge ucspi-ssl source from sysutils/ucspi-ssl.

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
 DATE=`date +%Y%m%d`
+$PORTS=/usr/ports
 SPAMCNTDIR=/usr/ports/mail/qmail-spamcontrol
 QMAILDIR=/usr/ports/mail/qmail
+UCSPIDIR=/usr/ports/sysutils/ucspi-ssl
 
 cp -p extra-patch-spamcontrol-tls_timeout.c $QMAILDIR/files
 mv $SPAMCNTDIR/Makefile $SPAMCONTDIR/Makefile.orig_$DATE
@@ -10,4 +12,5 @@ cp qmail-spamcontrol/Makefile $SPAMCNTDIR/Makefile
 mv $QMAILDIR/Makefile $QMAILDIR/Makefile.orig_$DATE
 cp qmail/Makefile $QMAILDIR/Makefile
 cd $SPAMCNTDIR && make makesum
+cp $PORTS/distfiles/ucspi-ssl\* $PORTS/distfiles/qmail
 
